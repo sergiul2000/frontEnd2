ebaae59247cd277fd60fe57a9882340e1f1b6e5f
sergiul2000 <68772846+sergiul2000@users.noreply.github.com>
2022-06-03 17:48:01 +0300

    Add files via upload

---
+++ api/SalaInterface.kt
@@ -0,0 +1,13 @@
+package com.example.helloworld.api
+
+//import retrofit2.Call
+
+interface SalaInterface {
+    /* @Get("sali")
+     fun getSali(): Call<List<SalaDataItem>>
+
+     @Post("sali")
+     fun addSala (@Body sala : SalaDataItem):Call<List<SalaDataItem>>
+     */
+
+}
